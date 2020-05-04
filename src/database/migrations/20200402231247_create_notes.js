7d25e675efa1a80e78e6f8cc1ef66ac27dd2d647
André Felipe Matos de Andrade <andrefmandrade@outlook.com>
2020-05-04 13:55:28 -0300

    correção migration de Notes

--- src/database/migrations/20200402231247_create_notes.js
+++ src/database/migrations/20200402231247_create_notes.js
@@ -4,7 +4,7 @@ exports.up = (knex) => {
 		table.string('title').notNullable()
 		table.string('content').notNullable()
 		table.date('created_at').notNullable().defaultTo(knex.fn.now())
-		table.date('updated_at').notNullable()
+		table.date('updated_at')
 
 		table.bigInteger('user_id').notNullable()
 		table.foreign('user_id').references('id').inTable('users')
