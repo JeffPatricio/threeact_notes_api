ff3d796d1cbe305f7efb5f97869309a8b3914edf
Jefferson Patrício <jeffersonpatricio1@gmail.com>
2020-05-05 22:23:32 -0300

    #6 implementação de recuperação de senha

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,10 @@ import express from 'express';
 import UserController from './controllers/UserController';
 import ActivationAccountController from './controllers/ActivationAccountController';
 import SessionController from './controllers/SessionController';
-import ForgetPasswordController from './controllers/ForgetPasswordController';
+import ForgotPasswordController from './controllers/ForgotPasswordController';
 import { requestLog } from './utils';
 import path from 'path';
+
 const routes = express.Router();
 
 routes.use('/public', express.static('public'));
@@ -12,10 +13,13 @@ routes.use(requestLog);
 
 // Rotas sem autenticação 
 routes.get('/', (_, res) => res.sendFile(path.resolve(__dirname, '..', 'public', 'status', 'status.html')));
+routes.get('/favicon.ico', (_, res) => res.sendFile(path.resolve(__dirname, '..', 'public', 'favicon.ico')));
 routes.get('/account/:token', ActivationAccountController.create);
 routes.post('/users', UserController.create);
 routes.post('/session', SessionController.create);
-routes.post('/forgetPassword', ForgetPasswordController.create);
+routes.post('/forgotPassword', ForgotPasswordController.create);
+routes.get('/forgotPassword/:token/:emailHash', ForgotPasswordController.read);
+routes.put('/forgotPassword', ForgotPasswordController.update);
 
 // Rotas com autenticação
 routes.get('/users/:userId', UserController.read);
