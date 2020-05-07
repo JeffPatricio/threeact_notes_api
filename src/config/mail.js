292eb59d879de315b33a1539ca71af1aa728ecda
Jefferson Patrício <jeffersonpatricio1@gmail.com>
2020-05-06 21:09:10 -0300

    commit deploy

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,16 +1,12 @@
 const nodemailer = require('nodemailer');
 require('dotenv/config');
 
-console.log('*************************************');
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
-
 module.exports = {
 	transporter: nodemailer.createTransport({
 		service: 'gmail',
 		auth: {
-			user: process.env.EMAIL_USER,
-			pass: process.env.EMAIL_PASS
+			user: `${process.env.EMAIL_USER}`,
+			pass: `${process.env.EMAIL_PASS}`
 		}
 	}),
 	options: {
